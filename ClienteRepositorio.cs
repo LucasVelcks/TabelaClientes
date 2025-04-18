531ec0da623477d332acaf261a1c87123906eff3
Lucas Velcks <lucasvelcks@gmail.com>
2025-04-17 21:37:07 -0300

    Criação dos fomularios de preenchimento

--- ClienteRepositorio.cs
+++ ClienteRepositorio.cs
@@ -30,10 +30,12 @@ namespace CrudClientes
 
                 cmd.ExecuteNonQuery();
             }
+
             catch (Exception ex) 
             {
                 MessageBox.Show("Erro ao inserir cliente: " + ex.Message);
             }
+
             finally 
             { 
                conexao.desconectar();
@@ -45,6 +47,7 @@ namespace CrudClientes
             SqlCommand select = new SqlCommand();
 
             List<Cliente> clientes = new List<Cliente>();
+
             try
             {
                 conexao.conectar();
@@ -66,27 +69,95 @@ namespace CrudClientes
                 }
                 return clientes;
             }
+
             catch (Exception ex)
             {
                 
             }
+
             finally
             {
                 conexao.desconectar();
             }
+
+            return clientes;
         }
         public void AtualizarCliente(Cliente cliente)
         {
+            Conexao conexao = new Conexao();
+            SqlCommand Update = new SqlCommand();
 
+            try
+            {
+                conexao.conectar();
+                string sql = "UPDATE Clientes SET (Nome = @Nome, Email = @Email, Telefone = @Telefone Where Id = @Id)";
+
+                Update.Parameters.AddWithValue("@Nome", cliente.Nome);
+                Update.Parameters.AddWithValue("@Email", cliente.Email);
+                Update.Parameters.AddWithValue("@Telefone", cliente.Telefone);
+                Update.Parameters.AddWithValue("@Id", cliente.Id);
+
+                Update.ExecuteNonQuery();
+            }
+
+            catch (Exception ex) 
+            {
+                MessageBox.Show("Erro ao atualizar cliente", ex.Message);
+            }
+
+            finally 
+            {
+                conexao.desconectar();
+            }
         }
 
         public void DeletarCliente(int Id)
         {
+            SqlCommand Deletar = new SqlCommand();
+            Conexao conexao = new Conexao();
+
+            try { 
+            conexao.conectar();
+            string sql = "DELETE FROM Clientes WHERE Id = @Id";
+
+            Deletar.Parameters.AddWithValue("@Id", Id);
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao deletar cliente", ex.Message);
+            }
+
+            finally 
+            { 
+            Deletar.ExecuteNonQuery();
+            }
 
         }
 
-        public void BuscarPorId(int Id)
+        public Cliente BuscarPorId(int Id)
         {
+            Conexao conexao = new Conexao();
+            SqlCommand Buscar = new SqlCommand();
+            string sql = "SELECT * FROM Clientes WHERE Id = @Id";
+
+            conexao.conectar();
+            Buscar.Parameters.AddWithValue("@Id", Id);
+
+            SqlDataReader reader = Buscar.ExecuteReader();
+
+            if (reader.Read()) 
+            {
+                Cliente clientes = new Cliente(
+                reader["Nome"].ToString(),
+                Convert.ToInt32(reader["Id"]),
+                reader["Email"].ToString(),
+                reader["Telefone"].ToString()
+                );
+
+                return clientes;
+            }
+            return null;
 
         }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,59 @@ namespace CrudClientes
         public Form1()
         {
             InitializeComponent();
+            InicializarComponentesCustomizados();
+        }
+
+        private void InicializarComponentesCustomizados()
+        {
+            Label lblNome = new Label();
+            lblNome.Text = "Nome";
+            lblNome.Location = new Point(20, 20);
+            lblNome.AutoSize = true;
+            this.Controls.Add(lblNome);
+
+            TextBox txtNome = new TextBox();
+            txtNome.Name = "txtNome";
+            txtNome.Location = new Point(80, 20);
+            txtNome.Width = 200;
+            this.Controls.Add(txtNome);
+
+            Label lblEmail = new Label();
+            lblEmail.Text = "Email";
+            lblEmail.Location = new Point(20, 60);
+            lblEmail.AutoSize = true;
+            this.Controls.Add(lblEmail);
+
+            TextBox txtEmail = new TextBox();
+            txtEmail.Name = "txtEmail";
+            txtEmail.Location = new Point(80, 58);
+            txtEmail.Width = 200;
+            this.Controls.Add(txtEmail);
+
+            Label lblTelefone = new Label();
+            lblTelefone.Text = "Telefone";
+            lblTelefone.Location = new Point(20, 100);
+            lblTelefone.AutoSize = true;
+            this.Controls.Add(lblTelefone);
+
+            TextBox txtTelefone = new TextBox();
+            txtTelefone.Name = "txtTelefone";
+            txtTelefone.Location = new Point(80, 98);
+            txtTelefone.Width = 200;
+            this.Controls.Add(txtTelefone);
+
+            Label lblId = new Label();
+            lblId.Text = "ID:";
+            lblId.Location = new Point(20, 0);
+            lblId.AutoSize = true;
+            this.Controls.Add(lblId);
+
+            TextBox txtId = new TextBox();
+            txtId.Name = "txtId";
+            txtId.Location = new Point(80, -2);
+            txtId.Width = 200;
+            txtId.Enabled = false;
+            this.Controls.Add(txtId);
         }
     }
 }
