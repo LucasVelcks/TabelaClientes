12a93143bdf500ac4b3a29bfee1d3f8c9e1a3b02
Lucas Velcks <lucasvelcks@gmail.com>
2025-04-19 20:27:55 -0300

    Atualização dos botões

--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace CrudClientes
 {
     public partial class Form1 : Form
     {
+        private TextBox txtNome;
+        private TextBox txtEmail;
+        private TextBox txtTelefone;
         public Form1()
         {
             InitializeComponent();
@@ -68,6 +72,92 @@ namespace CrudClientes
             txtId.Width = 200;
             txtId.Enabled = false;
             this.Controls.Add(txtId);
+
+            Button btnInserir = new Button();
+            btnInserir.Text = "Inserir";
+            btnInserir.Location = new Point(20, 160);
+            btnInserir.Click += BtnInserir_Click;
+            this.Controls.Add(btnInserir);
+
+            Button btnAtualizar = new Button();
+            btnAtualizar.Name = "Atualizar";
+            btnAtualizar.Location = new Point(120, 160);
+            btnAtualizar.Click += BtnAtualizar_Click;
+            this.Controls.Add(btnAtualizar);
+
+            Button btnBuscar = new Button();
+            btnBuscar.Name = "Buscar";
+            btnBuscar.Location = new Point(320, 160);
+            btnBuscar.Click += BtnBuscar_Click;
+            this.Controls.Add(btnBuscar);
+
+            Button btnExcluir = new Button();
+            btnExcluir.Name = "Excluir";
+            btnExcluir.Location = new Point(220, 160);
+            btnExcluir.Click += BtnExcluir_Click;
+            this.Controls.Add (btnExcluir);
+
+            Button btnListar = new Button();
+            btnListar.Text = "Listar";
+            btnListar.Location = new Point(120, 160);
+            btnListar.Click += BtnListar_Click;
+            this.Controls.Add(btnListar);
+
+            DataGridView dvgClientes = new DataGridView();
+            dvgClientes.Name = "dvgClientes";
+            dvgClientes.Location = new Point(20, 200);
+            dvgClientes.Size = new Size(500, 200);
+            dvgClientes.ReadOnly = true;
+            dvgClientes.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            this.Controls.Add(dvgClientes);
+
+        }
+
+        private void BtnInserir_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            string email = txtEmail.Text;
+            string telefone = txtTelefone.Text;
+            int id = 0;
+
+            Cliente novoCliente = new Cliente(nome, id, email, telefone);
+
+            ClienteRepositorio repositorio = new ClienteRepositorio();
+
+            repositorio.InserirCliente(novoCliente);
+
+            MessageBox.Show("Botão Inserir clicado!");
+
+            txtNome.Clear();
+            txtEmail.Clear();
+            txtTelefone.Clear();
+
+            ClienteRepositorio repositorio = new ClienteRepositorio();
+            List<Cliente> clientes = repositorio.ObterTodos();
+
+            string mensagem = "Clientes cadastros:\n\n";
+            foreach (var cliente in clientes)
+            {
+                mensagem += $"ID: {cliente.Id} \nNome: {cliente.Nome}\nEmail: {cliente.Email}\nTelefone: {cliente.Telefone}\n\n";
+            }
+
+            MessageBox.Show(mensagem);
+        }
+        private void BtnAtualizar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Botão Atualizar clicado!");
+        }
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Botão Buscar clicado!");
+        }
+        private void BtnExcluir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Botão Excluir clicado!");
+        }
+        private void BtnListar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Botão Listar clicado!");
         }
     }
 }
